4d1408d7543cd6786c49005d8d118d696726ea52
JavierTham <javiertjl99@gmail.com>
2022-06-06 15:10:35 +0800

    change mobilenet to alexnet

--- src/CNN_LSTM.py
+++ src/CNN_LSTM.py
@@ -46,11 +46,17 @@ class CNNLSTM(nn.Module):
     
     def __init__(self, lstm_hidden_size, lstm_num_layers):
         super(CNNLSTM, self).__init__()
-        self.cnn = torchvision.models.MobileNetV2().features
+        # self.cnn = torchvision.models.mobilenet_v2(pretrained=True).features
         # output of MobileNetv2 feature layer (1280, 7, 7)
-        self.AvgPool2d = nn.AvgPool2d(7)
+
+        cnn = torchvision.models.alexnet(pretrained=True)
+        self.cnn = nn.Sequential(*list(cnn.children())[:-1]) # remove last layer
+        # output of Alexnet 
+
+        self.AvgPool2d = nn.AvgPool2d(6)
         self.lstm = nn.LSTM(
-            1280,
+            # 1280,
+            256,
             lstm_hidden_size,
             lstm_num_layers,
             batch_first=True)
@@ -59,26 +65,45 @@ class CNNLSTM(nn.Module):
         # 157 classes
         self.fc2 = nn.Linear(256, 157)
         
+        # freeze entire CNN
         for param in self.cnn.parameters():
             param.requires_grad = False
                 
     def forward(self, x):
         # batch_size, sequence_length, num_channels, height, width
         B, L, C, H, W = x.size()
-        x = x.view(B * L, C, H, W)
         # CNN
-        x = self.cnn(x)
-        x = self.AvgPool2d(x)
-        x = x.view(x.size(0), -1)    # x.size(0): B*L
-        x = x.view(B, L, x.size(-1))
+
+        output = []
+        #loop over each frame
+        for i in range(L):
+            #input one frame at a time into the basemodel
+            x_t = self.cnn(x[:, i, :, :, :])
+            x_t = self.AvgPool2d(x_t)
+
+            # Flatten the output
+            x_t = x_t.view(x_t.size(0), -1)
+            #make a list of tensors for the given smaples 
+            output.append(x_t)
+        #end loop  
+        #make output as  (batch_size, sequence_length, output_size)
+        x = torch.stack(output, dim=0).transpose_(0, 1)
+        x = F.relu(x)
+        # x = x.view(B * L, C, H, W)
+        # x = self.cnn(x)
+        # x = self.AvgPool2d(x)
+        # x = x.view(x.size(0), -1)    # x.size(0): B*L
+        # x = x.view(B, L, x.size(-1))
+
         # LSTM
         x, (hn, cn) = self.lstm(x)
         x = x[:, -1, :].view(x.size(0), -1)
+        # FC
         x = self.fc1(x)
         x = F.relu(x)
-        # x = torch.sigmoid(x)
-        # FC
+        # x = torch.sigmoid(
         x = self.fc2(x)
+        # x = F.softmax(x, dim=0)
         
         return x
 
