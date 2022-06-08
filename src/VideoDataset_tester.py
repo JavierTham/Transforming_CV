264012f1916efd26b4fe7d191207f7d847eb4bfb
JavierTham <javiertjl99@gmail.com>
2022-06-08 13:37:36 +0800

    create tester for VideoDataset

---
+++ src/VideoDataset_tester.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+from functions import *
+from CNN_LSTM import *
+
+train_data_path = "../data/train_data.csv"
+video_path = "../data/Charades_v1"
+frames_path = "/media/kayne/SpareDisk/data/video_frames/"
+
+df = pd.read_csv(train_data_path).iloc[:1, :]
+frames_paths = list(df['id'].apply(lambda x: os.path.join(frames_path, f"{x}.pt")))
+vid_classes = np.asarray(df['vid_class'])
+
+dataset = VideoDataset(frames_paths, vid_classes)
+for data in dataset:
+	X, y = data[0], data[1]
+	print("Size/shape of frames:", X.shape)
+	print("Class:", y)
+	break
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+for data in dataset:
+	X, y = data[0], data[1]
+	print("X type:", type(X))
+	print("y type:", type(y))
+	X, y = X.to(device), y.to(device)
+	print("X device:", X.device)
+	print("y device:", y.device)
+	break
