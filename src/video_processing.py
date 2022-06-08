f615990fa3e7d67f0d97ea5ece682d4be94be6ec
JavierTham <javiertjl99@gmail.com>
2022-06-08 13:33:43 +0800

    modify transformation for video processing

--- src/video_processing.py
+++ src/video_processing.py
@@ -4,6 +4,7 @@ from PIL import Image
 import cv2
 import os
 import torch
+from torchvision import transforms
 
 import matplotlib.pyplot as plt
 
@@ -21,33 +22,44 @@ def process_video(video_id, video_path, seq_len=50):
 
         for frame_counter in range(seq_len):
             # Set the current frame position of the video, loop video if video too short
-            frame_position = frame_counter * skip_frames_window % video_frames_count 
+            frame_position = (frame_counter * skip_frames_window) % video_frames_count 
             video_reader.set(cv2.CAP_PROP_POS_FRAMES, frame_position)
             success, frame = video_reader.read() 
 
             if not success:
                 break
 
-            img = Image.fromarray(frame)
-            img = img.resize((256, 256))
-            img = img.crop((16, 16, 240, 240)) # center crop 224x224
-            frame = np.array(img)
-            frame[:,:,0] = (frame[:,:,0] - 0.485) / 0.229 # normalize mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-            frame[:,:,1] = (frame[:,:,1] - 0.456) / 0.224
-            frame[:,:,2] = (frame[:,:,2] - 0.406) / 0.225
+            # print(frame.shape)
+            # plt.imshow(frame)
+            # plt.show() 
 
-            frames_list.append(frame)
+            preprocess = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+
+            transformed_frame = preprocess(Image.fromarray(frame))
+
+            # plt.imshow(np.array(transformed_frame).transpose(1,2,0))
+            # plt.show()
+
+            frames_list.append(transformed_frame)
 
         video_reader.release()
-            
-        np.savez_compressed(f'/media/kayne/SpareDisk/data/video_frames/{video_id}.npz', frames_list)
+        
+        frames_list = torch.stack(frames_list, dim = 0)
+
+        torch.save(frames_list, f'/media/kayne/SpareDisk/data/video_frames/{video_id}.pt')
 
 train_data_path = "../data/train_data.csv"
 video_path = "../data/Charades_v1"
+frames_path = "/media/kayne/SpareDisk/data/video_frames/"
 
 df = pd.read_csv(train_data_path)
-vid_id_list = df['id']
+vid_id_list = df.loc[:, 'id'].tolist()
 
-# for vid_id in vid_id_list:
-#     print(vid_id)
-#     process_video(vid_id, video_path)
+for vid_id in vid_id_list[:2000]:
+    print(vid_id)
+    process_video(vid_id, video_path)
