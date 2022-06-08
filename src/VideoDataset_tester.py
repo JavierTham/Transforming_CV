eebdc4042a948ab73d6ac8d1ce78b06b65ca4ce9
JavierTham <javiertjl99@gmail.com>
2022-06-08 13:41:52 +0800

    clean up test for VideoDataset

--- src/VideoDataset_tester.py
+++ src/VideoDataset_tester.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import os
 import torch
-import torch.nn as nn
-from functions import *
 from CNN_LSTM import *
 
 train_data_path = "../data/train_data.csv"
@@ -15,15 +13,13 @@ frames_paths = list(df['id'].apply(lambda x: os.path.join(frames_path, f"{x}.pt"
 vid_classes = np.asarray(df['vid_class'])
 
 dataset = VideoDataset(frames_paths, vid_classes)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(frames_paths[0])
 for data in dataset:
 	X, y = data[0], data[1]
 	print("Size/shape of frames:", X.shape)
 	print("Class:", y)
-	break
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-for data in dataset:
-	X, y = data[0], data[1]
 	print("X type:", type(X))
 	print("y type:", type(y))
 	X, y = X.to(device), y.to(device)
