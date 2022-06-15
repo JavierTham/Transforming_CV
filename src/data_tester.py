dc34dadbfb58d6afab378fdd27f761c2159a7a8a
JavierTham <javiertjl99@gmail.com>
2022-06-15 15:57:59 +0800

    Refactor functions

--- src/data_tester.py
+++ src/data_tester.py
@@ -9,15 +9,14 @@ from tqdm import tqdm
 
 frames_path = "/media/kayne/SpareDisk/data/video_frames/"
 
-def test_data_class(video_id, video_class):
-    df = pd.read_csv("../data/train_data2.csv")
+def test_data_class(path, video_id, video_class):
+    df = pd.read_csv(path)
     action = df.iloc[video_id, -1]
     print(f"Correct class! {video_class}") if action == video_class\
                             else print(f"Wrong class, {action} not {video_class}")
 
-def test_data_shape(video_id, seq_len=50, channels=3, height=224, width=224):   
-    frames_path = "/media/kayne/SpareDisk/data/video_frames/"
-    path = os.path.join(frames_path, f"{video_id}.npz")
+def test_data_shape(path, video_id, seq_len=50, channels=3, height=224, width=224):       
+    path = os.path.join(path, f"{video_id}.npz")
     frames = np.load(path)[f"{video_id}.npy"]
 
     if frames.shape != (seq_len, channels, height, width):
@@ -25,11 +24,11 @@ def test_data_shape(video_id, seq_len=50, channels=3, height=224, width=224):
         return (video_id, frames.shape)
     return None
 
-def check_data(video_id):
+def check_data(path, video_id):
     df = pd.read_csv("../data/train_data2.csv")
     print(df.iloc[video_id, :])
 
-    path = os.path.join(frames_path, f"{video_id}.npz")
+    path = os.path.join(path, f"{video_id}.npz")
     frames = np.load(path)[f"{video_id}.npy"]
     
     for frame in frames:
@@ -37,11 +36,13 @@ def check_data(video_id):
         plt.show()
 
 if __name__ == "__main__":
-    num_examples = 3000
+    frames_path = "/media/kayne/SpareDisk/data/UCF101/video_frames/"
+
+    num_examples = 40
 
     wrong_data = []
-    for video_id in tqdm(range(1000, num_examples)):
-        output = test_data_shape(video_id)
+    for video_id in tqdm(range(num_examples)):
+        output = test_data_shape(frames_path, video_id)
         if output:
             wrong_data.append(output)
 
