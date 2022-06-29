e09d85dc90ee2ac179caa5c145af68a5c61533be
JavierTham <javiertjl99@gmail.com>
2022-06-29 10:01:38 +0800

    Rename arguments

--- src/CIFARDataset.py
+++ src/CIFARDataset.py
@@ -3,10 +3,10 @@ from torchvision import transforms
 from torch.utils.data import Dataset
 
 class CIFARDataset(Dataset):
-    def __init__(self, X_train, y_train):
+    def __init__(self, X, y):
         super(CIFARDataset, self).__init__()
-        self.X_train = X_train.reshape(len(X_train), 3, 32, 32) 
-        self.y_train = y_train
+        self.X = X.reshape(len(X), 3, 32, 32) 
+        self.y = y
         self.preprocess = transforms.Compose([
             transforms.Resize((384, 384)), # (384, 384) for fine tuning on ViT 
             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
@@ -14,10 +14,10 @@ class CIFARDataset(Dataset):
 
     def __getitem__(self, idx):        
         # scale to [0,1]
-        X = torch.tensor(self.X_train[idx] / 255, dtype=torch.float) 
+        X = torch.tensor(self.X[idx] / 255, dtype=torch.float) 
         X = self.preprocess(X)
-        y = torch.tensor(self.y_train[idx])
+        y = torch.tensor(self.y[idx])
         return X, y
     
     def __len__(self):
-        return len(self.X_train)
+        return len(self.X)
