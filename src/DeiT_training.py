4e7a1d0edd4ca69471e17060d6de6048122b3a5a
JavierTham <javiertjl99@gmail.com>
2022-07-05 11:24:30 +0800

    Create code to train DeiT

--- src/DeiT_training.py
+++ src/DeiT_training.py
@@ -1,4 +1,4 @@
-import numpy as np
+import timm
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -8,52 +8,40 @@ from functions import *
 from config.DeiT_config import *
 from ImagenetDataset import *
 
-import os
 import time
 import random
 import wandb
 
 random.seed(42)
 
-TRAIN_DATA_PATH = "/media/kayne/SpareDisk/data/imagenet/train"
-NUM_CLASSES = 1000
+TRAIN_DATA_PATH = "/media/kayne/SpareDisk/data/cifar100/train"
+NUM_CLASSES = 100
 
 time_now = time.strftime("%D %X")
-wandb.init(project="Transforming_CV", entity="javiertham", config=config, group="cifar", **{"name": "ViT_" + time_now})
+wandb.init(project="Transforming_CV", entity="javiertham", config=config, group="cifar", **{"name": "DeiT-tiny_" + time_now})
 wandb.config = config
 
-d = {"labels": [],
-    "data": np.empty((1281167, 3072)),
-    "mean": np.empty((1281167, 3072))}
-
-start_idx = 0
-end_idx = 0
-for i in range(1, 11):
-    train_path = os.path.join(TRAIN_DATA_PATH, f"train_data_batch_{i}")
-
-    train_data = unpickle(train_path)
-    labels = list(map(lambda x: x-1, train_data["labels"]))
-    d["labels"].extend(labels)
- 
-    data = train_data["data"]
-    means = train_data["mean"]
-
-    end_idx = start_idx + data.shape[0]
-    d["data"][start_idx:end_idx, :] = data
-    d["mean"][start_idx:end_idx, :] = means
-    start_idx += len(train_data)
-
-X = d["data"]
-y = d["labels"]
+train_data = unpickle(TRAIN_DATA_PATH)
+X_train = train_data["data"]
+y_train = train_data["fine_labels"]
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-deit = torch.hub.load('facebookresearch/deit:main', 'deit_tiny_patch16_224', pretrained=True)
+
+deit = timm.create_model(
+    'deit_tiny_patch16_224',
+    pretrained=True,
+    num_classes=NUM_CLASSES)
 deit.to(device)
+
+# freeze all layers except last
+for child in list(deit.children())[:-1]:
+    for param in child.parameters():
+        param.requires_grad = False
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(deit.parameters(), lr=config['learning_rate'])
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.20, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.20, random_state=42)
 
 train_dataset = ImagenetDataset(X_train, y_train)
 train_dataloader = DataLoader(train_dataset, **params)
