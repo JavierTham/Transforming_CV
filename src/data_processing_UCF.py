d50c777f5f0e3d9256ce2111c0768b79cc429a51
JavierTham <javiertjl99@gmail.com>
2022-07-14 09:55:48 +0800

    Update

--- src/data_processing_UCF.py
+++ src/data_processing_UCF.py
@@ -4,7 +4,7 @@ import cv2
 import re
 
 from sklearn import preprocessing
-from sklearn.model_selection import GroupShuffleSplit, train_test_split
+from sklearn.model_selection import GroupShuffleSplit
 
 VIDEO_PATH = "/media/kayne/SpareDisk/data/UCF101/videos/"
 LABELS = sorted(os.listdir(VIDEO_PATH))
@@ -29,31 +29,31 @@ def create_df(videos_path, classes):
 	df = pd.DataFrame(df, columns=["idx", "group_id", "path_to_vid", "action", "fps", "total_frame_count"])
 	return df
 
-def encode_labels(df, target, labels):
+def encode_labels(target, labels):
 	le = preprocessing.LabelEncoder()
 	le.fit(labels)
 	encoded_labels = le.transform(target)
 	return encoded_labels
 
 def split(df, *, X, y, train_size=None, test_size=None, group_id, random_state=42):
-	gss = GroupShuffleSplit(n_splits=1, train_size=train_size, test_size=test_size, random_state=42)
-	train_idx, test_idx = next(gss.split(df.loc[:, X], df.loc[:, y], groups=df[group_id]))
+	gss = GroupShuffleSplit(n_splits=1, train_size=train_size, test_size=test_size, random_state=random_state)
+	train_idx, test_idx = next(gss.split(df.loc[:, X], df.loc[:, y], groups=df.loc[:, group_id]))
 	return df.iloc[train_idx, :], df.iloc[test_idx, :]
 
 if __name__ == "__main__":
 	df = create_df(VIDEO_PATH, LABELS)
 
-	encoded_labels = encode_labels(df, df.loc[:, "action"], LABELS)
+	encoded_labels = encode_labels(df.loc[:, "action"], LABELS)
 
 	groups = df.loc[:, "group_id"].unique()
-	encoded_groups = encode_labels(df, df.loc[:, "group_id"], groups)
+	encoded_groups = encode_labels(df.loc[:, "group_id"], groups)
 	
 	df.loc[:, "action"] = encoded_labels
 	df.loc[:, "group_id"] = encoded_groups
 	# df.to_csv("../data/UCF_df.csv", index=False)
 
-	train_df, test_df = split(df, X="idx", y="idx", group_id="group_id", train_size=0.7)
-	val_df, test_df = split(test_df, X="idx", y="idx", group_id="group_id", test_size=0.5)
+	train_df, test_df = split(df, X="idx", y="action", group_id="group_id", train_size=0.7)
+	val_df, test_df = split(test_df, X="idx", y="action", group_id="group_id", test_size=0.5)
 	
 	train_df.to_csv("../data/UCF_train.csv", index=False)
 	val_df.to_csv("../data/UCF_val.csv", index=False)
