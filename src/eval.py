245839b6fa57751c3f0990164ee91adbbd77f7f1
JavierTham <javiertjl99@gmail.com>
2022-08-02 11:26:49 +0800

    Add logging of test score

--- src/eval.py
+++ src/eval.py
@@ -1,35 +1,54 @@
 import torch
+import wandb
 import pandas as pd
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-from functions import inv_normalize
+from functions import inv_normalize, unpickle
 
-all_y_true_path = "../output/DeiT/all_y_true.pt"
-all_y_pred_path = "../output/DeiT/all_y_pred.pt"
-picture_path = "../output/DeiT/pictures.pt"
-y_pred_examples_path = "../output/DeiT/y_pred_examples.pt"
-y_true_examples_path = "../output/DeiT/y_true_examples.pt"
+META_DATA_PATH = "/media/kayne/SpareDisk/data/cifar100/meta"
 
-all_y_true = torch.load(all_y_true_path)
-all_y_pred = torch.load(all_y_pred_path)
-pics = torch.load(picture_path)
-y_pred_examples = torch.load(y_pred_examples_path)
-y_true_examples = torch.load(y_true_examples_path)
+def get_accuracy(model_name):
+    all_y_true_path = f"../output/{model_name}/all_y_true.pt"
+    all_y_pred_path = f"../output/{model_name}/all_y_pred.pt"
+    picture_path = f"../output/{model_name}/pictures.pt"
+    y_pred_examples_path = f"../output/{model_name}/y_pred_examples.pt"
+    y_true_examples_path = f"../output/{model_name}/y_true_examples.pt"
 
-score = accuracy_score(all_y_true, all_y_pred)
-print("Accuracy score:", score)
+    all_y_true = torch.load(all_y_true_path)
+    all_y_pred = torch.load(all_y_pred_path)
+    pics = torch.load(picture_path)
+    y_pred_examples = torch.load(y_pred_examples_path)
+    y_true_examples = torch.load(y_true_examples_path)
 
-with open("/media/kayne/SpareDisk/data/imagenet/data_labels.txt", "r") as f:
-    data_labels = [s.split(" ") for s in f.readlines()]
+    print(y_pred_examples)
+    print(y_true_examples)
 
-with open("/media/kayne/SpareDisk/data/imagenet/actual_labels.txt", "r") as f:
-    actual_labels = [s.split(" ", 1)[1] for s in f.readlines()]
+    score = accuracy_score(all_y_true, all_y_pred)
+    return score
+    
 
-mapping = pd.read_csv("mapping.csv")
+    # labels = unpickle(META_DATA_PATH)["fine_label_names"]
+    
+    # for i in range(len(pics)):
+    #     pic = inv_normalize(pics[i])
+    #     print(y_true_examples[i])
+    #     print("y_true:\n", labels[y_true_examples[i]])
+    #     print("y_pred:\n", labels[y_pred_examples[i]])
+    #     plt.imshow(pic.permute(1,2,0))
+    #     plt.show()
 
-for i in range(len(pics)):
-    pic = inv_normalize(pics[i])
-    print("y_true:\n", actual_labels[y_true_examples[i]])
-    print("y_pred:\n", actual_labels[y_pred_examples[i]])
-    plt.imshow(pic.permute(1,2,0))
-    plt.show()
+# with open("/media/kayne/SpareDisk/data/imagenet/data_labels.txt", "r") as f:
+#     data_labels = [s.split(" ") for s in f.readlines()]
+
+# with open("/media/kayne/SpareDisk/data/imagenet/actual_labels.txt", "r") as f:
+#     actual_labels = [s.split(" ", 1)[1] for s in f.readlines()]
+
+# mapping = pd.read_csv("mapping.csv")
+
+if __name__ == "__main__":
+    model = "ResNet"
+    wandb.init(project="Transforming_CV", entity="javiertham", group="cifar", **{"name": f"{model}"})
+    score = get_accuracy(model)
+    wandb.log({"Test Accuracy": score})
+    print("Accuracy score:", score)
+    
