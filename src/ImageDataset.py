ab4b655d1a829b4a96ec86c1a8815aee0c4a4077
JavierTham <javiertjl99@gmail.com>
2022-08-12 14:23:19 +0800

    Move preprocessing out of ImageDataset class

--- src/ImageDataset.py
+++ src/ImageDataset.py
@@ -2,23 +2,16 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
-from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
 
 from PIL import Image
 
 class ImageDataset(Dataset):
-    def __init__(self, X, y, size=224):
+    def __init__(self, X, y, preprocess):
         super(ImageDataset, self).__init__()
         self.X = X.reshape(len(X), 3, 32, 32) 
         self.y = y
-        # standard image preprocessing for classification
-        # change accordingly
-        self.preprocess = transforms.Compose([
-            transforms.Resize(256, interpolation=InterpolationMode.BICUBIC),
-            transforms.CenterCrop(size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
-        ])
+        self.preprocess = preprocess
 
     def __getitem__(self, idx):
         X = self.preprocess(Image.fromarray(self.X[idx].transpose(1,2,0)))
