e092b15bcd7b766be154ba8d05e565ee09f66446
Javier Tham <javiertjl99@gmail.com>
2022-08-18 13:56:27 +0800

    move train from src to .

--- src/eval.py
+++ src/eval.py
@@ -2,7 +2,7 @@ import torch
 import wandb
 import pandas as pd
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
+
 from functions import inv_normalize, unpickle
 
 META_DATA_PATH = "/media/kayne/SpareDisk/data/cifar100/meta"
@@ -25,25 +25,7 @@ def get_accuracy(model_name):
 
     score = accuracy_score(all_y_true, all_y_pred)
     return score
-    
 
-    # labels = unpickle(META_DATA_PATH)["fine_label_names"]
-    
-    # for i in range(len(pics)):
-    #     pic = inv_normalize(pics[i])
-    #     print(y_true_examples[i])
-    #     print("y_true:\n", labels[y_true_examples[i]])
-    #     print("y_pred:\n", labels[y_pred_examples[i]])
-    #     plt.imshow(pic.permute(1,2,0))
-    #     plt.show()
-
-# with open("/media/kayne/SpareDisk/data/imagenet/data_labels.txt", "r") as f:
-#     data_labels = [s.split(" ") for s in f.readlines()]
-
-# with open("/media/kayne/SpareDisk/data/imagenet/actual_labels.txt", "r") as f:
-#     actual_labels = [s.split(" ", 1)[1] for s in f.readlines()]
-
-# mapping = pd.read_csv("mapping.csv")
 
 if __name__ == "__main__":
     model = "ResNet"
